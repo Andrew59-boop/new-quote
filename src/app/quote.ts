05b4ec29d4cbdd5fee0b94da9bd330fd99996207
Andrew59-boop <millerfaabi@gmail.com>
2020-06-13 09:39:10 +0300

    new commit

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+    public showDescription:boolean;
+    constructor(public person:string, public name:string ,public description:string, public completeDate:Date ){
+        this.showDescription=false
+     
+    }
+    
 }
